783b3ac8098359063774d1be5dfca66b9d7b54c1
DESKTOP-EKK8J81\njz <njz198869@aliyun.com>
2017-10-01 18:03:25 +0800

    提交typescript版本的贪食蛇

---
+++ src/components/Snake.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="snake">
+        <ul class="game">
+            <li class="row">
+                <ul class="row-container">
+                    <li class="col"></li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    import snake from '../../lib/typescript/dist/snake'
+    const {Game} = snake;
+
+    const rowCount = 10;
+    const colCount = 10;
+
+    export default {
+        data () {
+            return {
+                rowCount,
+                colCount,
+                game: new Game({
+
+                }),
+            }
+        }
+    }
+</script>
+
+<style scoped>
+</style>
