8ce65b554eea6f75431126c4561750c20947d15a
laden666666 <njz198869@gmail.com>
2018-04-14 22:59:25 +0800

    提交flow代码

--- src/page/language/Flow.vue
+++ src/page/language/Flow.vue
@@ -7,6 +7,9 @@
 <script>
     import Snake from '../../components/Snake.vue'
     const snake = require('../../../lib/flow/dist/snake')
+
+    console.log(snake)
+
     // const {Game} = snake;
 
     const rowCount = 10;
